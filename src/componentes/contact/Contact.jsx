834a47d6d77df1672a571d257d95fbe4e5ea3a6c
matt sb <mattsandovalb@gmail.com>
2022-07-07 12:38:20 +0200

    mpv commit

--- src/componentes/contact/Contact.jsx
+++ src/componentes/contact/Contact.jsx
@@ -1,34 +1,46 @@
 import React from "react";
 import "./Contact.css";
 
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faGithub, faInstagram, faLinkedin } from '@fortawesome/free-brands-svg-icons'
-import img3 from '../../img/contact.jpg';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faInstagram,
+  faLinkedin,
+} from "@fortawesome/free-brands-svg-icons";
+import img3 from "../../img/contact.jpg";
 
-function Contact (){
-    return(
-        <div>
-            <div className="general_contact">
-                <div className="contact_title">
-                    <h2>CONTACT ME</h2>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde quidem, nihil magnam consequatur ut deleniti veniam cupiditate tempora tenetur quos veritatis! Eligendi cum et fugiat voluptatum culpa reiciendis quod repudiandae!</p>
-                        <div className="decoration_one">
-                            
-                        </div>
-                </div>
-
-                    <div>
-                        <h2>mattsandovalbalada@gmail.com</h2>
-                        <a href="https://www.linkedin.com/feed/"><FontAwesomeIcon className="icon" icon={faLinkedin} /></a>
-                    <a href="https://www.instagram.com/matt.sandovalb/"><FontAwesomeIcon className="icon" icon={faInstagram} /></a>
-                    </div>
-            </div>
-
-            <div className="contact_img">
-                <img src={img3}  />
-                <div></div>
-            </div>
+function Contact() {
+  return (
+    <div>
+      <div className="general_contact">
+        <div className="contact_title">
+          <h2>CONTACT ME</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde
+            quidem, nihil magnam consequatur ut deleniti veniam cupiditate
+            tempora tenetur quos veritatis! Eligendi cum et fugiat voluptatum
+            culpa reiciendis quod repudiandae!
+          </p>
+          
+          <div className="decoration_one">
+            <hr className="top" />
+            <br />
+            <hr className="down" />
+          </div>
+          <h3>mattsandovalbalada@gmail.com</h3>
+          <div className="contact_div">
+            <a href="https://www.linkedin.com/feed/">
+              <FontAwesomeIcon className="icon" icon={faLinkedin} />
+            </a>
+            <a href="https://www.instagram.com/matt.sandovalb/">
+              <FontAwesomeIcon className="icon" icon={faInstagram} />
+            </a>
+          </div>
         </div>
-    )
+        <div className="contact_img">
+          <img src={img3} />
+        </div>
+      </div>
+    </div>
+  );
 }
 export default Contact;
